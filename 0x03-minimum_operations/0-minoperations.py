7c34e77f6e71f533fb18c18d825b350a67f6e1f3
Rmolimock <495@holbertonschool.com>
2020-02-26 21:13:27 +0000

    holberton algorithm project

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write
+a method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file.
+
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH =>
+Paste => HHHHHHHHH
+
+Number of operations: 6
+'''
+import math
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    for root in range(2, int(math.sqrt(n)) + 1):
+        if n % root == 0:
+            return minOperations(int(n / root)) + root
+    return n
