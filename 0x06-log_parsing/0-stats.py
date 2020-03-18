a7ea5df2a6ce60b7fba7794b9c07257be90235ad
Rmolimock <495@holbertonschool.com>
2020-03-18 23:51:34 +0000

    0-stats reads logs generated by 0-generator and parses them into useful information every 10 lines

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import stdin
+
+    file_size, count = 0, 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    try:
+        for line in stdin:
+            data = line.split()
+            ip = data[0]
+            status = data[-2]
+            size = data[-1]
+            if int(status) in status_codes:
+                status_codes[int(status)] += 1
+            file_size += int(size)
+            print("original: " + str(line))
+            count += 1
+            if count % 10:
+                print("File size: {}".format(file_size))
+                for code, value in sorted(status_codes.items()):
+                    if value > 0:
+                        print("{}: {}".format(code, value))
+
+    except KeyboardInterrupt:
+                print("File size: {}".format(file_size))
+                for code, value in sorted(status_codes.items()):
+                    if value > 0:
+                        print("{}: {}".format(code, value))
+                raise 
