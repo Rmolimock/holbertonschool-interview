f8815ecd61ce9f38bb752a4e4bde78b857d2b68e
Rmolimock <495@holbertonschool.com>
2020-03-19 00:03:32 +0000

    raising exceptions on input failure

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -8,13 +8,17 @@ if __name__ == "__main__":
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     try:
         for line in stdin:
-            data = line.split()
-            ip = data[0]
-            status = data[-2]
-            size = data[-1]
-            if int(status) in status_codes:
-                status_codes[int(status)] += 1
-            file_size += int(size)
+            try:
+                data = line.split()
+                status = data[-2]
+                if int(status) in status_codes:
+                    status_codes[int(status)] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(data[-1])
+            except BaseException:
+                pass
             count += 1
             if count % 10 == 0:
                 print("File size: {}".format(file_size))
