374cba5c7024fe35bc83a76c1b5a70a49cc6aed2
Rmolimock <495@holbertonschool.com>
2020-03-19 00:15:41 +0000

    refactored seperate print function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,32 +3,36 @@
 
 if __name__ == "__main__":
     from sys import stdin
-
-    file_size, count = 0, 0
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    file_size, count = 0, 0
+
+    def print_data(status_codes, file_size):
+        ''' print file size and status codes '''
+        print("File size: {}".format(file_size))
+        for k in sorted(status_codes.keys()):
+            if status_codes[k] > 0:
+                print("{}: {}".format(k, status_codes[k]))
     try:
         for line in stdin:
             try:
                 data = line.split()
-                status = data[-2]
-                if int(status) in status_codes:
-                    status_codes[int(status)] += 1
+                status_code = int(data[-2])
+
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
             except BaseException:
                 pass
             try:
                 file_size += int(data[-1])
             except BaseException:
                 pass
+
             count += 1
             if count % 10 == 0:
-                print("File size: {}".format(file_size))
-                for code, value in sorted(status_codes.items()):
-                    if value > 0:
-                        print("{}: {}".format(code, value))
+                print_data(status_codes, file_size)
 
     except KeyboardInterrupt:
-                print("File size: {}".format(file_size))
-                for code, value in sorted(status_codes.items()):
-                    if value > 0:
-                        print("{}: {}".format(code, value))
-                raise 
+        print_data(status_codes, file_size)
+        raise
+
+    print_data(status_codes, file_size)
