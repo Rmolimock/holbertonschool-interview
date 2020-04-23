51521cd74e00182d9ba98cfc96341558e5455a7b
Rmolimock <495@holbertonschool.com>
2020-04-23 07:11:02 +0000

    simplified algo

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,89 +1,35 @@
 #include "menger.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-char **memory(int size) {
-    int i;
-    char **square;
-
-    /* malloc string for height of square */
-    square = malloc(sizeof(char *) * size);
-    if (!square)
-        return (NULL);
-    for (i = 0; i < size; i++) {
-    /* malloc string for each row of square */
-        square[i] = malloc(sizeof(char *) * size);
-    /* if malloc fails for any row, free it along with previous rows */
-        if (!square[i]) {
-            while (i >= 0) {
-                free(square[i]);
-                i--;
-            }
-            free(square);
-            return (NULL);
-        }
-    }
-    return (square);
-}
-
-void free_square(char **square, int size) {
-    while (size--)
-        free(square[size]);
-    free(square);
-}
-
-int edge_cases(int n) {
-    if (!n) {
-        printf("#\n");
-        return (1);
-    }
-    if (n < 0) {
-        return (1);
-    }
-    return (0);
+/**
+ * _char - selects for block or no block
+ * @col: column
+ * @row: row
+ * Return: '#' or ' '
+ */
+char find_char(int col, int row)
+{
+	for (; col && row; col /= 3, row /= 3)
+		if (col % 3 == 1 && row % 3 == 1)
+			return (' ');
+	return ('#');
 }
 
 /**
- *print_square - prints the menger buffer to stdo
- *@square: matrix of symbols representing menger square
- *@size: height and width of the square
- *
- *Return: Void.
+ * menger - prints 2 dimensional menger square
+ * @level: size of square
  */
-void print_square(char **square, int size) {
-    int y, x;
+void menger(int level)
+{
+	int col, row, size;
 
-    for (y = 0; y < size; y++) {
-        for (x = 0; x < size; x++) {
-            printf("%c", square[y][x]);
-        }
-        printf("\n");
-    }
-    square++;
+	if (level >= 0)
+	{
+		size = pow(3, level);
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+				printf("%c", find_char(col, row));
+			printf("\n");
+		}
+	}
 }
 
-
-/**
- *menger - create a 2d menger square and print it to stdo
- *@n: size of square
- *
- *Return: Void.
- */
-void menger(int n) {
-    char **square;
-    int size = (int)pow(3, n);
-    int y, x = 0;
-
-    if (edge_cases(n))
-        return;
-    if (!(square = memory(size)))
-        return;
-    for (y = 0; y < size; y++) {
-        for (x = 0; x < size; x++) {
-            printf("#");
-        }
-    }
-    print_square(square, size);
-    free_square(square, size);
-}
--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -2,5 +2,10 @@
 #define MENGER_H
 
 void menger(int level);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+
 
 #endif
