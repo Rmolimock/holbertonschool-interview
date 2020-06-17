1e6f429e20854c3f0de193ecfdf64fa2186af06d
Rmolimock <495@holbertonschool.com>
2020-06-17 10:29:36 +0000

    incrementing water once each time instead and looping several times

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,23 +4,19 @@
 
 def rain(walls):
     """ rain algorithm """
-    if not walls:
-        return 0
-    if not isinstance(walls, list):
-        return 0
-    if len(walls) == 0:
-        return 0
-    left, right, water, space = 0, 0, 0, False
-    for wall in walls:
-        if wall and not left:
-            left = wall
-            space = False
-        elif wall and left:
-            if space:
-                right = wall
-                water += left if left < right else right
-            left = wall
-            space = False
+    left = right = water = 0
+    i, end = 0, len(walls) - 1
+    while i < end:
+        if walls[end] < walls[i]:
+            if walls[end] > right:
+                right = walls[end]
+            else:
+                water += right - walls[end]
+            end -= 1
         else:
-            space = True
+            if left > walls[i]:
+                water += left - walls[i]
+            else:
+                left = walls[i]
+            i += 1
     return water
