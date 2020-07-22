21b7e28eb3b41c2b7d03cd6e3fae65236a73bca4
Rmolimock <495@holbertonschool.com>
2020-07-22 22:08:40 +0000

    rotate matrix clockwise

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,24 +3,20 @@
 rotate matrix counter clockwise
 """
 
-matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
 
-def rotate_2d_matrix(mat): 
-    N = len(mat)
-    for x in range(0, int(N / 2)): 
-          
-        for y in range(x, N-x-1): 
-              
-            temp = mat[x][y] 
-  
-            mat[x][y] = mat[y][N-1-x] 
-  
-            mat[y][N-1-x] = mat[N-1-x][N-1-y] 
-  
-            mat[N-1-x][N-1-y] = mat[N-1-y][x] 
-  
-            mat[N-1-y][x] = temp 
-  
- 
+def rotate_2d_matrix(matrix):
+    """
+    rotate matrix counter clockwise
+    """
+    if not len(matrix) or len(matrix) != len(matrix[0]):
+        return
+    n = len(matrix)
+    for y in range(n // 2):
+        x, end, offset = y, n - 1 - y, 0
+        for i in range(x, end):
+            tmp = matrix[x][i]
+            matrix[x][i] = matrix[end - offset][x]
+            matrix[end - offset][x] = matrix[end][end - offset]
+            matrix[end][end - offset] = matrix[i][end]
+            matrix[i][end] = tmp
+            offset += 1
