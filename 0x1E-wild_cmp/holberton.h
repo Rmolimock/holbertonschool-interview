7eb5ee2556294c28fba187c68ac5d764c6ee96b1
Russell Molimock <495@holbertonschool.com>
2021-07-16 02:26:38 +0000

    deleting old work I didn't realize was still there

--- 0x1E-wild_cmp/holberton.h
+++
@@ -1,5 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-int wildcmp(char *s1, char *s2);
-
-#endif /* HOLBERTON_H */
--- 0x20-regex/regex.h
+++
@@ -1,7 +0,0 @@
-#ifndef _REGEX_H
-#define _REGEX_H
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-int regex_match(char const *str, char const *pattern);
-#endif
